b1c1cc536b000091edf2efc7f5c7f30d96935e2e
k-kimura <kimura_511@me.com>
2019-04-08 08:35:13 +0900

    ボタンを押した時の処理の実装

--- monsterslayer/app.js
+++ monsterslayer/app.js
@@ -13,27 +13,61 @@
         this.monsterHealth = 100;
       },
       attack: function() {
-        let max = 10;
-        let min = 3;
-        let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+        let damage = this.calculateDamage(3, 10);
         this.monsterHealth -= damage;
-
-        if (this.monsterHealth <= 0) {
-          alert('You won');
-          this.gameIsRunning = false;
+        // こういうコードの実行をしているのは2回目の処理用の対策
+        // trueならもうやめてしまう
+        if (this.checkWin()) {
+          return;
         }
-        // 参考通りに書くかけどこの書き方いいのかね？ ミュータブルで
-        max = 12;
-        min = 5;
-        damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-        this.playerHealth -= damage;
-        if (this.playerHealth <= 0) {
-          alert('You lost');
-          this.gameIsRunning = false;
+        // メソッド名の前にthisを忘れがちなので注意
+        this.monsterAttacks();
+      },
+      specialAttack: function() {
+        let damage = this.calculateDamage(10, 20);
+        this.monsterHealth -= damage;
+        if (this.checkWin()) {
+          return;
+        }
+        this.monsterAttacks();
+      },
+      heal: function() {
+        if (this.playerHealth <= 90) {
+          this.playerHealth += 10;
+        } else {
+          this.playerHealth = 100;
         }
       },
-      specialAttack: function() {},
-      heal: function() {},
-      giveUp: function() {}
+      giveUp: function() {
+        this.gameIsRunning = false;
+      },
+      // specialアタックと普通のアタックの処理は同じ記載なので１つにまとめる
+      monsterAttacks: function() {
+        damage = this.calculateDamage(5, 12);
+        this.playerHealth -= damage;
+        this.checkWin();
+      },
+      // ダメージ計算の共通化
+      calculateDamage: function(min, max) {
+        return Math.max(Math.floor(Math.random() * max) + 1, min);
+      },
+      checkWin: function() {
+        if (this.monsterHealth <= 0) {
+          if (confirm('You won! New Game?')) {
+            this.startGame();
+          } else {
+            this.gameIsRunning = false;
+          }
+          return true;
+        } else if (this.playerHealth <= 0) {
+          if (confirm('You lost! New Game?')) {
+            this.startGame();
+          } else {
+            this.gameIsRunning = false;
+          }
+          return true;
+        }
+        return false;
+      }
     }
   });
